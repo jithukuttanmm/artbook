0e267b62c45941230c597085240927f94361108d
Jithu <jithukuttanmm@outlook.com>
2021-07-22 07:58:15 +0530

    Update index.js

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(express.static(path.join("public")));
+
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  });
+
+app.listen(3000,()=>{console.log('Connected successfully!');});
